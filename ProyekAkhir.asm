9f4683f9d925bcb1e919bab3a8c1eb1041506663
Fata Nugraha <fatanugraha@outlook.com>
2017-06-05 17:57:52 +0700

    add change time button

--- ProyekAkhir.asm
+++ ProyekAkhir.asm
@@ -5,12 +5,31 @@
 
 .include "m8515def.inc"
 
+.def time_info=r14
+.def day_info=r15
+
 .def tmp=r16
+.def tmp2=r17
+
 .def arg_1=r24
 .def arg_2=r25
 
 .org $00
 rjmp main
+.org $01
+rjmp int_next_shift
+.org $02
+rjmp int_prev_shift
+
+int_prev_shift:
+	rcall led_prev_shift
+	rcall led_update
+	reti
+
+int_next_shift:
+	rcall led_next_shift
+	rcall led_update
+	reti
 
 main:
 	;init stack
@@ -24,7 +43,14 @@ main:
 	;clar lcd
 	rcall lcd_clear
 	
-	
+	;init time
+	rcall led_init
+	ldi tmp, 1
+	mov time_info, tmp
+	mov day_info, tmp
+	rcall led_update
+
+	;show welcome screen
 	ldi arg_1, low(msg_welcome_row1*2)
 	ldi arg_2, high(msg_welcome_row1*2)
 	rcall lcd_show_string
@@ -35,10 +61,83 @@ main:
 	ldi arg_1, low(msg_welcome_row2*2)
 	ldi arg_2, high(msg_welcome_row2*2)
 	rcall lcd_show_string
+	
+	;init interrupts
+	ldi tmp, (1<<INT1)|(1<<INT0)
+	out GIMSK, tmp
+	ldi tmp, (1<<ISC01)|(1<<ISC11)
+	out MCUCR, tmp
+	sei
 
 forever:
 	rjmp forever
 
+;led functions
+led_prev_shift:
+	mov tmp, time_info
+	lsr tmp
+	tst tmp
+	brne led_prev_shift_no_mod	
+	ldi tmp, 4
+	led_prev_shift_no_mod:
+		mov time_info, tmp
+
+	cpi tmp, 4
+	brne led_prev_shift_no_change
+
+	mov tmp, day_info
+	lsr tmp
+	tst tmp
+	brne led_prev_shift_day_no_mod
+	ldi tmp, 16
+	led_prev_shift_day_no_mod:
+		mov day_info, tmp
+
+	led_prev_shift_no_change:
+		ret
+
+led_next_shift:
+	mov tmp, time_info
+	lsl tmp
+	cpi tmp, 8
+	brne led_next_shift_no_mod	
+	ldi tmp, 1
+	led_next_shift_no_mod:
+		mov time_info, tmp
+
+	cpi tmp, 1
+	brne led_next_shift_no_change
+
+	mov tmp, day_info
+	lsl tmp
+	cpi tmp, 32
+	brne led_next_shift_day_no_mod
+	ldi tmp, 1
+	led_next_shift_day_no_mod:
+		mov day_info, tmp
+
+	led_next_shift_no_change:
+		ret
+
+led_init:
+	ser tmp
+	out DDRC, tmp
+	ldi tmp, 0 
+	out PORTC, tmp
+	ret
+
+led_update:
+	mov tmp, time_info
+	lsl tmp
+	lsl tmp
+	lsl tmp
+	lsl tmp	
+	lsl tmp
+	or tmp, day_info
+	out PORTC, tmp
+	ret
+
+;lcd functions
 lcd_init:	
 	;init 8bit, 2line, 5x7	
 	ldi arg_1, $38	
